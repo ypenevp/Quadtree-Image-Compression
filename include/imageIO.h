c4e64e107af86b465436939c72aa594f6f0e0376
ypenevp <ypenevp@gmail.com>
2026-06-06 21:26:34 +0300

    feat(imageIO): add read/write functions for .ppm images

---
+++ include/imageIO.h
@@ -0,0 +1,42 @@
+#ifndef IMAGEIO_H
+#define IMAGEIO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MEMCHECK(ptr)                         \
+    if (ptr == NULL)                          \
+    {                                         \
+        printf("Error allocating memory!!!\n"); \
+        exit(1);                              \
+    }
+
+#define FILECHECK(ptr, filename)                              \
+    if (ptr == NULL)                                          \
+    {                                                         \
+        printf("Error opening file '%s'!!!\n", filename);      \
+        exit(1);                                              \
+    }
+
+typedef struct
+{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+} Pixel;
+
+typedef struct
+{
+    int width;
+    int height;
+    Pixel *data;
+} Image;
+
+Image initImage(int width, int height);
+void releaseImage(Image *img);
+
+Image readPPM(const char *filename);
+void writePPM(const char *filename, Image *img);
+
+#endif
---
+++ src/imageIO.c
@@ -0,0 +1,58 @@
+#include "imageIO.h"
+
+Image initImage(int width, int height)
+{
+    Image img;
+    img.width = width;
+    img.height = height;
+    img.data = NULL;
+
+    if (width > 0 && height > 0)
+    {
+        img.data = calloc(width * height, sizeof(Pixel));
+        MEMCHECK(img.data);
+    }
+
+    return img;
+}
+
+void releaseImage(Image *img)
+{
+    free(img->data);
+    img->data = NULL;
+    img->width = 0;
+    img->height = 0;
+}
+
+////////////////////////////////////////////////////
+
+Image readPPM(const char *filename)
+{
+    FILE *file = fopen(filename, "rb");
+    FILECHECK(file, filename);
+
+    char format[3];
+    fscanf(file, "%2s", format);
+
+    int width = 0, height = 0, maxColor = 0;
+    fscanf(file, "%d %d %d", &width, &height, &maxColor);
+    fgetc(file); // read last \n before binary pixels data
+
+    Image img = initImage(width, height);
+    fread(img.data, sizeof(Pixel), width * height, file);
+
+    fclose(file);
+    return img;
+}
+
+void writePPM(const char *filename, Image *img)
+{
+    FILE *file = fopen(filename, "wb");
+    FILECHECK(file, filename);
+
+    fprintf(file, "P6\n%d %d\n255\n", img->width, img->height);
+
+    fwrite(img->data, sizeof(Pixel), img->width * img->height, file);
+
+    fclose(file);
+}
